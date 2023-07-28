9221b942b488a98592e632246a18aab8fc551edf
rakibul58 <rhrahi14@gmail.com>
2023-07-28 11:29:03 +0600

    add course

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 
+app.use(express.static('front'))
+
+
 const port = 5000
 
 app.use(cors());
@@ -33,6 +36,12 @@ async function run() {
         const userCollection = client.db('experiment-labs').collection('allusers');
         const userdetailsCollection = client.db('experiment-labs').collection('userdetails');
         const redeem_categoriesCollection = client.db('experiment-labs').collection('redeem_categories');
+        const gamification_SettingsCollection = client.db('experiment-labs').collection('gamification_Settings');
+        const earnings_logicCollection = client.db('experiment-labs').collection('earnings_logic');
+        const item_earning_parameterCollection = client.db('experiment-labs').collection('item_earning_parameter');
+        const item_redemption_parameter_internalCollection = client.db('experiment-labs').collection('item_redemption_parameter_internal');
+
+        const courseCollection = client.db('experiment-labs').collection('courses');
 
 
         //create organization
@@ -51,7 +60,14 @@ async function run() {
             }
             const result = await orgCollection.insertOne(user);
             res.send(result)
-        })
+        });
+
+        app.get('/createOrganization', async (req, res) => {
+            const query = {}
+            const allorg = await orgCollection.find(query).toArray();
+
+            res.send(allorg);
+        });
 
         //create user
         app.post('/createUser', async (req, res) => {
@@ -67,26 +83,17 @@ async function run() {
         })
 
         //login
-        app.get('/login', async (req, res) => {
+        app.post('/login', async (req, res) => {
             const user = req.body;
+            console.log(user);
 
-            let email = await orgCollection.findOne({ email: user.email, password: user.password });
+            let email = await orgCollection.findOne({ email: user.email });
 
             if (email) {
-                return res.json({ "login successful": "admin" })
+                return res.send({ role: "admin" });
             }
-            let useremail = await userCollection.findOne({ email: user.email, password: user.password });
-
-            if (useremail) {
-                return res.json({ "login successful": "user" })
-            }
-
-            else {
-                return res.status(400).json({ error: " user not found" })
-            }
-            // const result = await usersCollection.insertOne(user);
-            // res.send(result)
-        })
+            return res.send({ role: "user" });
+        });
 
         // add to organization a user
         app.post('/addToOrganization', async (req, res) => {
@@ -121,57 +128,70 @@ async function run() {
             }
             const result = await userdetailsCollection.insertOne(filter);
             res.send(result)
-        })
+
+
+
+
+        });
 
         //add to organization by new user
-        app.post('/addToOrganizationBynewadmin', async (req, res) => {
-            let query = {};
-            const email = req.query.email;
+        // app.post('/addToOrganizationBynewadmin', async (req, res) => {
+        //     let query = {};
+        //     const email = req.query.email;
 
-            const user = req.body;
+        //     const user = req.body;
 
-            let adminEmail = await userdetailsCollection.findOne({ user_email: email });
+        //     let adminEmail = await userdetailsCollection.findOne({ user_email: email });
 
 
-            /* if (!adminEmail) {
-                return res.status(400).json({ error: " admin not found" })
-            } */
-            let newAdmin = await userdetailsCollection.findOne({ user_type: "admin", user_email: email });
-            if (!newAdmin) {
-                return res.status(400).json({ error: " admin not found" })
-            }
-            const id = user.org_id;
+        //     /* if (!adminEmail) {
+        //         return res.status(400).json({ error: " admin not found" })
+        //     } */
+        //     let newAdmin = await userdetailsCollection.findOne({ user_type: "admin", user_email: email });
+        //     if (!newAdmin) {
+        //         return res.status(400).json({ error: " admin not found" })
+        //     }
+        //     const id = user.org_id;
 
-            let org_id = await orgCollection.findOne({ org_id: id });
-            if (!org_id) {
-                return res.status(400).json({ error: " Organization id not found" })
-            }
+        //     let org_id = await orgCollection.findOne({ org_id: id });
+        //     if (!org_id) {
+        //         return res.status(400).json({ error: " Organization id not found" })
+        //     }
 
-            let actEmail = await userCollection.findOne({ email: user.user_email });
-            if (!actEmail) {
-                return res.status(400).json({ error: "sorry user not found" })
-            }
-            let checkUser = await userdetailsCollection.findOne({ user_email: user.user_email });
-            if (checkUser) {
-                return res.status(400).json({ error: "sorry a user already exixts" })
-            }
-            const userEmai = user.user_email;
-            const type = user.user_type;
-            const filter = {
-                org_id: id,
-                user_email: userEmai,
-                user_type: type
-            }
-            const result = await userdetailsCollection.insertOne(filter);
-            res.send(result)
-        })
+        //     let actEmail = await userCollection.findOne({ email: user.user_email });
+        //     if (!actEmail) {
+        //         return res.status(400).json({ error: "sorry user not found" })
+        //     }
+        //     let checkUser = await userdetailsCollection.findOne({ user_email: user.user_email });
+        //     if (checkUser) {
+        //         return res.status(400).json({ error: "sorry a user already exixts" })
+        //     }
+        //     const userEmai = user.user_email;
+        //     const type = user.user_type;
+        //     const filter = {
+        //         org_id: id,
+        //         user_email: userEmai,
+        //         user_type: type
+        //     }
+        //     const result = await userdetailsCollection.insertOne(filter);
+        //     res.send(result)
+        // })
 
-        //add redeem_categories
-        app.post('/redeem_categories', async (req, res) => {
-            const user = req.body;
-            const result = await redeem_categoriesCollection.insertOne(user);
-            res.send(result)
-        })
+
+
+        // //add redeem_categories
+        // app.post('/redeem_categories', async (req, res) => {
+        //     const user = req.body;
+        //     const result = await redeem_categoriesCollection.insertOne(user);
+        //     res.send(result)
+        // });
+
+
+        // app.get('/redeem_categories', async (req, res) => {
+        //     const query = {}
+        //     const redeem_categories = await redeem_categoriesCollection.find(query).toArray();
+        //     res.send(redeem_categories);
+        // });
 
 
         app.get('/addToOrganization', async (req, res) => {
@@ -194,6 +214,95 @@ async function run() {
             const collections = await userdetailsCollection.find(query).toArray();
             res.send(collections);
         });
+
+
+        //create Gamification_Settings
+        app.post('/gamification_settings', async (req, res) => {
+            const settings = req.body;
+            const result = await gamification_SettingsCollection.insertOne(settings);
+            res.send(result)
+        });
+
+
+        app.get('/gamification_settings', async (req, res) => {
+            const query = {}
+            const settings = await gamification_SettingsCollection.find(query).toArray();
+            res.send(settings);
+        });
+
+
+        //earnings_logic
+        app.post('/earnings_logic', async (req, res) => {
+            const earnings_logic = req.body;
+            const result = await earnings_logicCollection.insertOne(earnings_logic);
+            res.send(result)
+        });
+
+
+        app.get('/earnings_logic', async (req, res) => {
+            const query = {}
+            const earnings_logic = await earnings_logicCollection.find(query).toArray();
+            res.send(earnings_logic);
+        });
+
+        //item_earning_parameter
+        app.post('/item_earning_parameter', async (req, res) => {
+            const item_earning_parameter = req.body;
+            const result = await item_earning_parameterCollection.insertOne(item_earning_parameter);
+            res.send(result)
+        });
+
+
+        app.get('/item_earning_parameter', async (req, res) => {
+            const query = {}
+            const item_earning_parameter = await item_earning_parameterCollection.find(query).toArray();
+            res.send(item_earning_parameter);
+        });
+
+
+        //redemption_logic
+        app.post('/redemption_logic', async (req, res) => {
+            const user = req.body;
+            const result = await redeem_categoriesCollection.insertOne(user);
+            res.send(result)
+        });
+
+
+        app.get('/redemption_logic', async (req, res) => {
+            const query = {}
+            const redeem_categories = await redeem_categoriesCollection.find(query).toArray();
+            res.send(redeem_categories);
+        });
+
+
+        //item_redemption_parameter_internal
+        app.post('/item_redemption_parameter_internal', async (req, res) => {
+            const item_redemption_parameter_internal = req.body;
+            const result = await item_redemption_parameter_internalCollection.insertOne(item_redemption_parameter_internal);
+            res.send(result)
+        });
+
+
+        app.get('/item_redemption_parameter_internal', async (req, res) => {
+            const query = {}
+            const item_redemption_parameter_internal = await item_redemption_parameter_internalCollection.find(query).toArray();
+            res.send(item_redemption_parameter_internal);
+        });
+
+        //get courses
+        app.get('/courses', async (req, res) => {
+            const query = {}
+            const courses = await courseCollection.find(query).toArray();
+            res.send(courses);
+        });
+
+        //add courses
+        app.post('/courses', async (req, res) => {
+            const course = req.body;
+            const result = await courseCollection.insertOne(course);
+            res.send(result)
+        });
+
 
         //user add
 
